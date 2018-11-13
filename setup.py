9f76e603b5db39728076c4ea810732667812562b
Henrik Palmlund Wahlgren <henrik@pwit.se>
2018-11-13 09:29:15 +0100

    work for first version

--- setup.py
+++ setup.py
@@ -1,4 +1,67 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+import sys
+import os
+from shutil import rmtree
+
+# Package meta-data.
+NAME = 'utilitarian-queue-consumer'
+DESCRIPTION = "A micro framework for writing consumers of AMR-UM or other AMQP messages."
+URL = 'https://github.com/pwitab/utilitarian-queue-consumer'
+EMAIL = 'henrik@pwit.se'
+AUTHOR = "Henrik Palmlund Wahlgren @ Palmlund Wahlgren Innovative Technology AB"
+REQUIRES_PYTHON = '>=3.6'
+# VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'kombu',
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system(
+            '{0} setup.py sdist bdist_wheel'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        # os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -6,27 +69,35 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    'kombu'
-]
-
 setup(
-    name='utilitarian-queue-consumer',
-    version='0.0.1dev',
-    python_requires='~=3.6',
-    description="A micro framework for writing consumers of AMR-UM or other AMQP messages.",
+    name=NAME,
+    version='0.0.1',
+    python_requires=REQUIRES_PYTHON,
+    description=DESCRIPTION,
     long_description=readme + '\n\n' + history,
-    author="Henrik Palmlund Wahlgren @ Palmlund Wahlgren Innovative Technology AB",
-    author_email='henrik@pwit.se',
-    url='https://www.pwit.se',
-    packages=find_packages(),
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    entry_points={},
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
     include_package_data=True,
-    install_requires=requirements,
-
-    license='BSD 3-Clause License',
+    license="BSD-3",
     zip_safe=False,
-    keywords='',
+    keywords=[],
     classifiers=[
-
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
--- utilitarian_queue_consumer/workers.py
+++ utilitarian_queue_consumer/workers.py
@@ -3,7 +3,16 @@ from kombu.mixins import ConsumerProducerMixin, ConsumerMixin
 from utilitarian_queue_consumer.conf import settings
 
 
+class UtilitarianMessageHandlerError(Exception):
+    """An exception occured in """
+
+
 class UtilitarianConsumer(ConsumerMixin):
+    """
+    Base class for consumers that are running in Utilitarian
+    Define the handle_message method to process a message in a
+    subclass of this class.
+    """
 
     def __init__(self, connection, exchanges, queues):
         self.connection = connection
@@ -19,11 +28,27 @@ class UtilitarianConsumer(ConsumerMixin):
         return consumer
 
     def on_message(self, message):
-        pass
+        # Kombu has an issue that if an error is an OSError comes in consumers
+        # code it will silently eat the error and never raise an exception:
+        # https://github.com/celery/kombu/issues/802
+        # Workaround for now is to wrap all consumer code in a general
+        # try/except and raise an exception if something happens inside the
+        # consumer code.
+
+        try:
+            self.handle_message(message)
+        except Exception as e:
+            raise UtilitarianMessageHandlerError() from e
+
+    def handle_message(self, message):
+        raise NotImplementedError('This needs to be implemented in subclass')
 
 
 class UtilitarianProducingConsumer(ConsumerProducerMixin, UtilitarianConsumer):
-    """"""
-    pass
+    """
+    With the producer mixinclass the consumer gets a producer that can publish
+    messages available under self.producer
+    """
 
-
+    def handle_message(self, message):
+        raise NotImplementedError('This needs to be implemented in subclass')
